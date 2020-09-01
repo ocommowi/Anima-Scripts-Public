b870e6fa3a3f3f97cc080a4ad4e3c8ca432ecf7b
Olivier Commowick <olivier.commowick@inria.fr>
2020-09-01 11:23:16 +0200

    Results main folder added

--- registration_evaluation/animaDiffusionRegistrationEvaluation.py
+++ registration_evaluation/animaDiffusionRegistrationEvaluation.py
@@ -191,7 +191,7 @@ tracksList = ['AF_left', 'AF_right', 'ATR_left', 'ATR_right', 'CA', 'CC_1', 'CC_
 
 transformsList = ['movingAnat_aff', 'movingAnat_nl', 'movingTensors_nl', 'movingMCM_nl', 'movingAnatTensors_nl', 'movingAnatMCM_nl', 'movingTensorsMCM_nl', 'movingAnatTensorsMCM_nl']
 
-resultsDir = os.path.join(dataDir, 'Results_' + str(refIndex) + '_' + str(movIndex))
+resultsDir = os.path.join(dataDir, 'Results', 'Results_' + str(refIndex) + '_' + str(movIndex))
 if os.path.exists(resultsDir):
     shutil.rmtree(resultsDir)
 
